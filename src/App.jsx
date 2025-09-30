5f1ff077e9dbabb6400bd81dac2c251bcd1973cf
Hoon <avbalonzo30@gmail.com>
2025-09-30 18:21:34 +0800

    edit app

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { Home } from './pages/home';
 import { Pagenotfound } from './pages/pagenotfound';
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path='*' element={<Pagenotfound />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="*" element={<Pagenotfound />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
